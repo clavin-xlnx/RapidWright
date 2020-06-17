c8b3d4a51f280067f938666deef434aacab56306
Pongstorn Maidee <pongsto@xlnx.xilinx.com>
2020-06-16 17:31:08 -0700

    remove empty files

--- src/com/xilinx/rapidwright/timing/delayestimator/DiscoverVertex.java
+++
@@ -1,7 +0,0 @@
-package com.xilinx.rapidwright.timing.delayestimator;
-
-import org.jgrapht.Graph;
-
-//public interface DiscoverVertex<V, E> {
-//    double apply(Graph<V,E> g, V v, E e, Double d);
-//}
--- src/com/xilinx/rapidwright/timing/delayestimator/ExamineEdge.java
+++
@@ -1,8 +0,0 @@
-package com.xilinx.rapidwright.timing.delayestimator;
-
-
-import org.jgrapht.Graph;
-
-//public interface ExamineEdge<V,E> {
-//    void apply(Graph<V,E> g, V v, E e, Double val);
-//}
