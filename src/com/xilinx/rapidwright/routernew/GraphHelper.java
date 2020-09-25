3cee9ee0277728e3bc16a4d520c79ae13b7a8502
YunxZhou <Yun.Zhou@ugent.be>
2020-09-25 15:59:34 +0200

    changed to deal with nodes

--- src/com/xilinx/rapidwright/routernew/GraphHelper.java
+++ src/com/xilinx/rapidwright/routernew/GraphHelper.java
@@ -2,14 +2,16 @@ package com.xilinx.rapidwright.routernew;
 
 import java.util.*;
 
+import com.xilinx.rapidwright.device.Node;
+
 public class GraphHelper{
-	private List<Routable> vertices;
-	private Map<Routable, Set<Routable>> routableEdges;
-	Map<Routable, Boolean> visited;
-	Map<Routable, Boolean> stack;
+	private List<Node> vertices;
+	private Map<Node, Set<Node>> nodeEdges;
+	Map<Node, Boolean> visited;
+	Map<Node, Boolean> stack;
 	
-	Routable backEdgeStart;
-	Routable backEdgeEnd;
+	Node backEdgeStart;
+	Node backEdgeEnd;
 
 	boolean debug = false;
 	
@@ -19,7 +21,7 @@ public class GraphHelper{
 		
 	public boolean isCyclic(Netplus netp){
 		this.vertices = new ArrayList<>();
-		this.routableEdges = new HashMap<>();
+		this.nodeEdges = new HashMap<>();
 		this.visited = new HashMap<>();
 		this.stack = new HashMap<>();
 		
@@ -27,32 +29,32 @@ public class GraphHelper{
 		
 //		if(netp.getNet().getName().equals("ipr[9]"))this.debug = true;
 		
-		return this.isCyclic(this.vertices, this.routableEdges, this.visited, this.stack);
+		return this.isCyclic(this.vertices, this.nodeEdges, this.visited, this.stack);
 	}
 	public void buildGraph(Netplus netp){
 		for(Connection c:netp.getConnection()){
 			this.addVertices(c);
-			int vertexSize = c.rnodes.size();
+			int vertexSize = c.nodes.size();
 			for(int i = vertexSize - 1; i > 0; i--){
-				Routable fisrt = c.rnodes.get(i);
-				Routable second = c.rnodes.get(i - 1);
+				Node fisrt = c.nodes.get(i);
+				Node second = c.nodes.get(i - 1);
 				
-				if(!routableEdges.containsKey(fisrt)){
-					Set<Routable> redges = new HashSet<>();
+				if(!nodeEdges.containsKey(fisrt)){
+					Set<Node> redges = new HashSet<>();
 					redges.add(second);
-					this.routableEdges.put(fisrt, redges);
+					this.nodeEdges.put(fisrt, redges);
 				}else{
-					Set<Routable> redges = this.routableEdges.get(fisrt);
+					Set<Node> redges = this.nodeEdges.get(fisrt);
 					redges.add(second);
-					this.routableEdges.put(fisrt, redges);
+					this.nodeEdges.put(fisrt, redges);
 				}
 			}
 		}
 	}
 	
 	public void addVertices(Connection c){
-		for(int i = c.rnodes.size() - 1; i >= 0; i-- ){
-			Routable rn = c.rnodes.get(i);
+		for(int i = c.nodes.size() - 1; i >= 0; i-- ){
+			Node rn = c.nodes.get(i);
 			if(!this.vertices.contains(rn)){
 				this.vertices.add(rn);
 				this.visited.put(rn, false);
@@ -61,7 +63,7 @@ public class GraphHelper{
 		}
 	}
 	
-	public boolean isCyclicKernel(Routable rn, Map<Routable, Boolean> visited, Map<Routable, Boolean> stack){
+	public boolean isCyclicKernel(Node rn, Map<Node, Boolean> visited, Map<Node, Boolean> stack){
 		if(stack.get(rn)){
 			if(this.debug) this.printlnInfo("rn stack true " + rn.toString());
 			return true;
@@ -72,8 +74,8 @@ public class GraphHelper{
 		visited.put(rn, true);
 		stack.put(rn, true);
 		
-		if(this.routableEdges.get(rn) != null){
-			for(Routable child:this.routableEdges.get(rn)){
+		if(this.nodeEdges.get(rn) != null){
+			for(Node child:this.nodeEdges.get(rn)){
 				
 				if(this.debug) this.printlnInfo("rn " + rn.toString() + ",  child " + child.toString());
 				this.backEdgeStart = rn;
@@ -91,12 +93,12 @@ public class GraphHelper{
 		return false;
 	}
 	
-	public boolean isCyclic(List<Routable> vertices, 
-			Map<Routable, Set<Routable>> routableEdges,
-			Map<Routable, Boolean> visited,
-			Map<Routable, Boolean> stack){
+	public boolean isCyclic(List<Node> vertices, 
+			Map<Node, Set<Node>> routableEdges,
+			Map<Node, Boolean> visited,
+			Map<Node, Boolean> stack){
 		
-		for(Routable rn:this.vertices){
+		for(Node rn:this.vertices){
 			if(this.debug) this.printlnInfo("starting from rn " + rn.toString());
 			if(this.isCyclicKernel(rn, this.visited, this.stack))
 				return true;
@@ -110,22 +112,22 @@ public class GraphHelper{
 			//TODO to store info of the back edge
 //			System.out.println("start " + this.backEdgeStart.toString() + ", end" + this.backEdgeEnd.toString());
 			// remove the back edge by removing the end from the adj list of the start
-			for(Routable rn:this.routableEdges.keySet()){
+			for(Node rn:this.nodeEdges.keySet()){
 				if(rn.equals(this.backEdgeStart)){
-					this.routableEdges.get(rn).remove(backEdgeEnd);
+					this.nodeEdges.get(rn).remove(backEdgeEnd);
 					break;
 				}
 			}
 			
 			// check if the start of the back edge has any fanouts
-			// if no, remove the redundant routable and remove edges related to it
-			if(this.routableEdges.get(this.backEdgeStart).size() == 0){
+			// if no, remove the redundant Node and remove edges related to it
+			if(this.nodeEdges.get(this.backEdgeStart).size() == 0){
 				
 				this.removeRedudantRoutable(this.backEdgeStart);
 				
-				for(Routable rn:this.routableEdges.keySet()){
-					if(this.routableEdges.get(rn).contains(this.backEdgeStart)){
-						this.routableEdges.get(rn).remove(backEdgeStart);
+				for(Node rn:this.nodeEdges.keySet()){
+					if(this.nodeEdges.get(rn).contains(this.backEdgeStart)){
+						this.nodeEdges.get(rn).remove(backEdgeStart);
 					}
 				}
 			}
@@ -133,74 +135,74 @@ public class GraphHelper{
 			// reset the info of visited and stack for each routable, preparing for the next check if the graph is cyclic
 			this.resetVistedAndStack();
 			this.debug = false;
-			cycleExists = this.isCyclic(this.vertices, this.routableEdges, this.visited, this.stack);	
-			System.out.println(cycleExists);
+			cycleExists = this.isCyclic(this.vertices, this.nodeEdges, this.visited, this.stack);
+			if(!cycleExists) System.out.println("cycles removed");
 		}
 		
 		this.rebuildPathsOfNetCons(netp);
 	}
 	
-	public void removeRedudantRoutable(Routable backEdgeStart){
-		if(this.debug) System.out.println("before removing " + this.vertices.size() + ", " + this.routableEdges.size() + ", " + this.visited.size() + ", " +this.stack.size());
+	public void removeRedudantRoutable(Node backEdgeStart){
+		if(this.debug) System.out.println("before removing " + this.vertices.size() + ", " + this.nodeEdges.size() + ", " + this.visited.size() + ", " +this.stack.size());
 		this.vertices.remove(backEdgeStart);
-		this.routableEdges.remove(backEdgeStart);
+		this.nodeEdges.remove(backEdgeStart);
 		this.visited.remove(backEdgeStart);
 		this.stack.remove(backEdgeStart);
-		if(this.debug) System.out.println("after removing " + this.vertices.size() + ", " + this.routableEdges.size() + ", " + this.visited.size() + ", " +this.stack.size());
+		if(this.debug) System.out.println("after removing " + this.vertices.size() + ", " + this.nodeEdges.size() + ", " + this.visited.size() + ", " +this.stack.size());
 		
 	}
 	
 	public void resetVistedAndStack(){
-		for(Routable rn:this.vertices){
+		for(Node rn:this.vertices){
 			this.resetVisited(rn);
 			this.resetStack(rn);
 		}
 	}
 	
-	public void resetVisited(Routable rn){
+	public void resetVisited(Node rn){
 		this.visited.put(rn, false);
 	}
-	public void resetStack(Routable rn){
+	public void resetStack(Node rn){
 		this.stack.put(rn, false);
 	}
 	
 	public void rebuildPathsOfNetCons(Netplus netp){
 		// TODO identify connections that have been impacted
 		for(Connection c:netp.getConnection()){
-			List<Routable> path = this.findPathBetweenTwoVertices(c.getSourceRNode(), c.getSinkRNode());
-			c.rnodes.clear();
+			List<Node> path = this.findPathBetweenTwoVertices(c.getSourceRNode().getNode(), c.getSinkRNode().getNode());
+			c.nodes.clear();
 			for(int i = 0; i < path.size(); i++){
-				c.rnodes.add(path.get(i));
+				c.nodes.add(path.get(i));
 				if(this.debug) this.printlnInfo(path.get(i).toString());
 			}
 		}
 	}
 	
-	public List<Routable> findPathBetweenTwoVertices(Routable source, Routable sink){
-		List<Routable> path = new ArrayList<>();
+	public List<Node> findPathBetweenTwoVertices(Node source, Node sink){
+		List<Node> path = new ArrayList<>();
 		// set visited value of each vertex as false
-		for(Routable rn:this.vertices){
+		for(Node rn:this.vertices){
 			this.resetVisited(rn);
 		}
 		if(this.debug) System.out.println("sink " + sink.toString());
-		Queue<Routable> queue = new LinkedList<>();
-		Map<Routable, Routable> childParent = new HashMap<>();
+		Queue<Node> queue = new LinkedList<>();
+		Map<Node, Node> childParent = new HashMap<>();
 		this.visited.put(source, true);
 		queue.add(source);
 		while(!queue.isEmpty()){
-			Routable rn = queue.poll();
+			Node rn = queue.poll();
 			if(rn.equals(sink)){
 				//TODO trace back to build the path
-				Routable tmp = rn;
+				Node tmp = rn;
 				while(tmp != null){
 					path.add(tmp);
 					tmp = childParent.get(tmp);
 				}
 			}
 			if(this.debug) System.out.println(rn.toString());
-			if(this.debug) System.out.println(this.routableEdges.containsKey(rn));
-			if(this.routableEdges.containsKey(rn)){// SINKRR is not contained
-				for(Routable child:this.routableEdges.get(rn)){
+			if(this.debug) System.out.println(this.nodeEdges.containsKey(rn));
+			if(this.nodeEdges.containsKey(rn)){// SINKRR is not contained
+				for(Node child:this.nodeEdges.get(rn)){
 					if(!this.visited.get(child)){
 						this.visited.put(child, true);
 						childParent.put(child, rn);
