fef0b3a67a351775ddb1f77271721774223d6bae
YunxZhou <Yun.Zhou@ugent.be>
2020-12-30 17:14:17 +0100

    rewriting findThePIPbetweenTwoNodes() and removing unused methods

--- src/com/xilinx/rapidwright/routernew/RouterHelper.java
+++ src/com/xilinx/rapidwright/routernew/RouterHelper.java
@@ -7,17 +7,14 @@ import com.xilinx.rapidwright.design.Net;
 import com.xilinx.rapidwright.design.SitePinInst;
 import com.xilinx.rapidwright.device.Node;
 import com.xilinx.rapidwright.device.PIP;
-import com.xilinx.rapidwright.device.Tile;
-import com.xilinx.rapidwright.device.Wire;
-import com.xilinx.rapidwright.timing.ImmutableTimingGroup;
 
 public class RouterHelper {
-	//TODO a collections of methods for routing functionality
+	//a collections of methods for routing
 	public RouterHelper(){
 		
 	}
 	
-	//methods to check the net
+	//methods to categorize the net
 	public static boolean isRoutableNetWithSourceSinks(Net n){
 		return n.getSource() != null && n.getSinkPins().size() > 0;
 	}
@@ -42,12 +39,6 @@ public class RouterHelper {
 		return source.getName().equals("COUT") && (!sink.getName().equals("CIN"));
 	}
 	
-	public static boolean isCyclicRoutingTree(Net n){
-		boolean isCyclic = false;
-		
-		return isCyclic;
-	}
-	
 	//methods for pips assignment
 	public static List<PIP> conPIPs(Connection con){
 		List<PIP> conPIPs = new ArrayList<>();
@@ -57,7 +48,7 @@ public class RouterHelper {
 			Node nodeFormer = ((RoutableNode) (con.rnodes.get(i))).getNode();
 			Node nodeLatter = ((RoutableNode) (con.rnodes.get(i-1))).getNode();
 			
-			PIP pip = findThePIPbetweenTwoNodes(nodeFormer.getAllWiresInNode(), nodeLatter);
+			PIP pip = findThePIPbetweenTwoNodes(nodeFormer, nodeLatter);
 			if(pip != null){
 				conPIPs.add(pip);
 			}else{
@@ -70,12 +61,13 @@ public class RouterHelper {
 	public static List<PIP> conPIPs(List<Node> conNodes){
 		List<PIP> conPIPs = new ArrayList<>();
 		
-		//nodes of a connection are added starting from the sink until the source
+		//nodes of a connection are added starting from the sink to the source
 		for(int i = conNodes.size() -1; i > 0; i--){
 			Node nodeFormer = conNodes.get(i);
 			Node nodeLatter = conNodes.get(i-1);
 			
-			PIP pip = findThePIPbetweenTwoNodes(nodeFormer.getAllWiresInNode(), nodeLatter);
+			PIP pip = findThePIPbetweenTwoNodes(nodeFormer, nodeLatter);
+			
 			if(pip != null){
 				conPIPs.add(pip);
 			}else{
@@ -85,48 +77,25 @@ public class RouterHelper {
 		return conPIPs;
 	}
 	
-	public static PIP findThePIPbetweenTwoNodes(Wire[] nodeFormerWires, Node nodeLatter){
+	public static PIP findThePIPbetweenTwoNodes(Node nodeFormer, Node nodeLatter) {
 		PIP pip = null;
-		Tile pipTile = nodeLatter.getTile();
-		int wire1 = nodeLatter.getWire();
-		for(Wire wire:nodeFormerWires){
-			if(wire.getTile().equals(pipTile)){
-				pip = pipTile.getPIP(wire.getWireIndex(), wire1);
-				if(pip != null){
-					 break;
-				}
-			}	
-		}
-		return pip;
-	}
-	
-	public static ImmutableTimingGroup findImmutableTimingGroup(RoutableTimingGroup rtgFirst, RoutableTimingGroup rtgSecond){
-		if(rtgSecond.getSiblingsTimingGroup().getSiblings().length > 1){
-			Node exitofFirst = rtgFirst.getSiblingsTimingGroup().getExitNode();
-			ImmutableTimingGroup[] immus = rtgSecond.getSiblingsTimingGroup().getSiblings();
-			int length = immus.length;
-			for(int i = 0; i < length; i++){
-				Node entryOfSecond = immus[i].entryNode();
-//				System.out.println(exitofFirst.toString() + ", " + entryOfSecond.toString());
-				if(twoNodesAbutted(exitofFirst, entryOfSecond)){
-					return immus[i];
-				}
+		List<PIP> nodeFormerPIPs = nodeFormer.getAllDownhillPIPs();
+		for(PIP p : nodeFormerPIPs) {
+			if(p.getEndNode().equals(nodeLatter)) {
+				return p;
 			}
-		}else{
-			return rtgSecond.getSiblingsTimingGroup().getSiblings()[0];
 		}
 		
-		System.err.println("Null ImmutableTimingGroup found connecting two SiblingsTimingGroup");
-		return null;
-	}
-	
-	public static boolean twoNodesAbutted(Node exitOfFirst, Node entryOfSecond){//NodeWithFaninInfo
-		for(Node n:exitOfFirst.getAllDownhillNodes()){
-//			System.out.println(n.toString());
-			if(n.toString().equals(entryOfSecond.toString()))//
-				return true;
+		if(nodeFormer.toString().contains("BYPASS")) {
+			nodeFormerPIPs = nodeFormer.getAllUphillPIPs();
+			for(PIP p : nodeFormerPIPs) {
+				if(p.getStartNode().equals(nodeLatter)) {
+					return p;
+				}
+			}
 		}
-		return false;
+		
+		return pip;
 	}
 	
 }
