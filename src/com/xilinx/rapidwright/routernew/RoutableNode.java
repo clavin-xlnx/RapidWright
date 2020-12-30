91635766976b4c8e323a520abff120922814f32e
YunxZhou <Yun.Zhou@ugent.be>
2020-12-30 17:15:01 +0100

    removing unused import

--- src/com/xilinx/rapidwright/routernew/RoutableNode.java
+++ src/com/xilinx/rapidwright/routernew/RoutableNode.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import com.xilinx.rapidwright.design.SitePinInst;
 import com.xilinx.rapidwright.device.Node;
 import com.xilinx.rapidwright.device.PIP;
 import com.xilinx.rapidwright.device.Tile;
