8ccf7e73a4ee14a5bf8c26a4a9fae7f01d24c085
YunxZhou <Yun.Zhou@ugent.be>
2020-12-31 09:52:56 +0100

    findPIPs methods

--- src/com/xilinx/rapidwright/routernew/RouterHelper.java
+++ src/com/xilinx/rapidwright/routernew/RouterHelper.java
@@ -7,6 +7,8 @@ import com.xilinx.rapidwright.design.Net;
 import com.xilinx.rapidwright.design.SitePinInst;
 import com.xilinx.rapidwright.device.Node;
 import com.xilinx.rapidwright.device.PIP;
+import com.xilinx.rapidwright.device.Tile;
+import com.xilinx.rapidwright.device.Wire;
 
 public class RouterHelper {
 	//a collections of methods for routing
@@ -77,24 +79,43 @@ public class RouterHelper {
 		return conPIPs;
 	}
 	
-	public static PIP findThePIPbetweenTwoNodes(Node nodeFormer, Node nodeLatter) {
-		PIP pip = null;
-		List<PIP> nodeFormerPIPs = nodeFormer.getAllDownhillPIPs();
-		for(PIP p : nodeFormerPIPs) {
-			if(p.getEndNode().equals(nodeLatter)) {
-				return p;
-			}
+	public static PIP findThePIPbetweenTwoNodes(Node nodeFormer, Node nodeLatter){
+		PIP pip = findPIP(nodeLatter.getTile(), nodeFormer.getAllWiresInNode(), nodeLatter.getWire());
+		
+//		if(pip == null) {
+//			pip = findPIP(nodeFormer.getTile(), nodeLatter.getAllWiresInNode(), nodeFormer.getWire());
+//		}
+		if(pip == null) {//for other scenarios, such as LAG tile nodes, LAG_LAG_X12Y250/LAG_MUX_ATOM_0_TXOUT to node LAG_LAG_X12Y310/UBUMP0 
+			pip = findPIP(nodeFormer, nodeLatter);
 		}
 		
-		if(nodeFormer.toString().contains("BYPASS")) {
-			nodeFormerPIPs = nodeFormer.getAllUphillPIPs();
-			for(PIP p : nodeFormerPIPs) {
-				if(p.getStartNode().equals(nodeLatter)) {
-					return p;
+		return pip;
+	}
+	
+	public static PIP findPIP(Tile pipTile, Wire[] wires, int endWire) {
+		PIP pip = null;
+		for(Wire wire : wires){
+			if(wire.getTile().equals(pipTile)){
+				pip = pipTile.getPIP(wire.getWireIndex(), endWire);
+				if(pip != null){
+					break;
 				}
 			}
 		}
-		
+		return pip;
+	}
+	
+	//this will return null for NodeGroup router, because nodeFormer is Node, NodeLatter is NodeWithFaninInfo
+	public static PIP findPIP(Node nodeFormer, Node nodeLatter) {
+		PIP pip = null;
+		for(PIP p : nodeFormer.getAllDownhillPIPs()) {
+			if(p.getEndNode().equals(nodeLatter))
+				return p;
+		}
+		for(PIP p : nodeFormer.getAllUphillPIPs()) {
+			if(p.getStartNode().equals(nodeLatter))
+				return p;
+		}
 		return pip;
 	}
 	
